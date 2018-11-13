741285b6cea4f692d003db2e194f037474b92263
dataspectsTestGitUserName <dataspectsTestGitUserName@dataspects.com>
2018-11-13 10:15:14 +0000

    WIP

--- test_dataspects.sh
+++ test_dataspects.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-DATASPECTS_VERSION=181109b
+# [[IsTestScriptFor::dataspectsApp]]
+
+DATASPECTS_VERSION=181113a
 
 docker run \
+  --network mydataspectsstandardsystem181112a_default \
   --volume /media/lex/LEXSAMSUNG-64GB/dataspects_docker_image_factory:/usr/dataspects_docker_image_factory \
-  --volume /media/lex/LEXSAMSUNG-64GB/ProvisionDSAsDSCookbook:/usr/ProvisionDSAsDSCookbook \
-  --workdir /usr/dataspects_docker_image_factory/dataspects_lib \
-  --network dataspectsstandardsystem_default \
-  --env PROFILES=/usr/ProvisionDSAsDSCookbook/config/standard_system_profiles.yml \
-  --env SHOW_DATASPECTS_LOG=false \
+  --volume ${PWD}:/usr/workspace \
   --rm \
   --tty \
+  --env SHOW_DATASPECTS_LOG=false \
+  --env PROFILES=/usr/workspace/config/standard_system_profiles.yml \
+  --workdir /usr/dataspects_docker_image_factory/dataspects_lib \
     dataspects/dataspects:$DATASPECTS_VERSION \
       rspec spec/extract_from_MW_and_inject_into_MW_spec.rb
+
+# alias watchgitstatus="watch --color 'pwd; git -c color.status=always status'"
