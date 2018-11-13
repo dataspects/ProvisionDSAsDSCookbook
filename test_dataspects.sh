c46b3361f7a03a64330c9238ffe682426cbc0f02
dataspectsTestGitUserName <dataspectsTestGitUserName@dataspects.com>
2018-11-13 12:19:11 +0100

    Enhanced test

--- test_dataspects.sh
+++ test_dataspects.sh
@@ -7,11 +7,12 @@ DATASPECTS_VERSION=181113a
 docker run \
   --network mydataspectsstandardsystem181112a_default \
   --volume /media/lex/LEXSAMSUNG-64GB/dataspects_docker_image_factory:/usr/dataspects_docker_image_factory \
-  --volume ${PWD}:/usr/workspace \
+  --volume ${PWD}:/usr/config \
+  --volume ${PWD}/..:/usr/workspace \
   --rm \
   --tty \
   --env SHOW_DATASPECTS_LOG=false \
-  --env PROFILES=/usr/workspace/config/standard_system_profiles.yml \
+  --env PROFILES=/usr/config/config/standard_system_profiles.yml \
   --workdir /usr/dataspects_docker_image_factory/dataspects_lib \
     dataspects/dataspects:$DATASPECTS_VERSION \
       rspec spec/extract_from_MW_and_inject_into_MW_spec.rb
